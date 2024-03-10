d0da37e477b9246ca4224d238e03179b66a5cd32
Tino <tino.muzambi@uct.ac.za>
2024-03-10 13:18:02 +0200

    Adds energy.

--- World Map/prepareData.js
+++ World Map/prepareData.js
@@ -10,25 +10,37 @@ streams.forEach((d) => {
 	const country = d.country;
 	const streams = parseInt(d.streams);
 	const danceability = parseFloat(d.danceability);
+	const energy = parseFloat(d.energy);
 
 	// If first entry for country, initialize to 0
 	if (!byCountry[country]) {
-		byCountry[country] = { streams: 0, danceability: 0, danceabilityTotal: 0 };
+		byCountry[country] = {
+			streams: 0,
+			danceability: 0,
+			danceabilityTotal: 0,
+			energy: 0,
+			energyTotal: 0,
+		};
 	}
 
 	// Increment country's total streams
 	byCountry[country].streams += streams;
 	byCountry[country].danceabilityTotal += 1;
 	byCountry[country].danceability += danceability;
+	byCountry[country].energyTotal += 1;
+	byCountry[country].energy += energy;
 });
 
 Object.keys(byCountry).forEach((country) => {
 	byCountry[country].danceability =
 		byCountry[country].danceability / byCountry[country].danceabilityTotal;
+	byCountry[country].energy =
+		byCountry[country].energy / byCountry[country].energyTotal;
 });
 
 Object.keys(byCountry).forEach((country) => {
 	delete byCountry[country].danceabilityTotal;
+	delete byCountry[country].energyTotal;
 });
 
 console.log(byCountry);
