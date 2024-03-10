1432441f828acf2b1e4b175fb1aa1902408fcfd7
Tino <tino.muzambi@uct.ac.za>
2024-03-10 08:46:13 +0200

    Converts object structure to include danceability.

--- World Map/prepareData.js
+++ World Map/prepareData.js
@@ -2,25 +2,24 @@ import streams from "./data/spotify.json" assert { type: "json" };
 import fs from "fs";
 
 // Initialize totals map
-const totalStreamsByCountry = {};
+const byCountry = {};
 
 // Loop through each data entry
 streams.forEach((d) => {
 	// Extract the country and streams
 	const country = d.country;
 	const streams = parseInt(d.streams);
+	const danceability = parseFloat(d.danceability);
 
 	// If first entry for country, initialize to 0
-	if (!totalStreamsByCountry[country]) {
-		totalStreamsByCountry[country] = 0;
+	if (!byCountry[country]) {
+		byCountry[country] = { streams: 0, danceability: 0 };
 	}
 
 	// Increment country's total streams
-	totalStreamsByCountry[country] += streams;
+	byCountry[country].streams += streams;
+	byCountry[country].danceability += danceability;
 });
 
-console.log(totalStreamsByCountry);
-fs.writeFileSync(
-	"./data/streamsByCountry.json",
-	JSON.stringify(totalStreamsByCountry)
-);
+console.log(byCountry);
+fs.writeFileSync("./data/byCountry.json", JSON.stringify(byCountry));
